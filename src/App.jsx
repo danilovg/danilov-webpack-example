c5aa6b20fdd68ac62730ac3d35d4e2171726cf33
Danilov_G <gleb22d@mail.ru>
2023-12-01 20:04:20 +0200

    rollup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+const App = () => {
+   return (
+    <>
+        <h1>Rollup</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="cake" />
+    </>
+   );
+}
+
+export default App;
